a35d0ea5a8402bc03a5e99627bff911223ce63b9
lebalz <lebalz@outlook.com>
2021-05-15 16:56:31 +0200

    update pips

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -57,6 +57,13 @@ c.DockerSpawner.volumes = {
 c.Spawner.cpu_limit = 2
 c.Spawner.mem_limit = '2G'
 
+# shutdown the server after no activity for an hour
+c.NotebookApp.shutdown_no_activity_timeout = 60 * 60
+# shutdown kernels after no activity for 20 minutes
+c.MappingKernelManager.cull_idle_timeout = 20 * 60
+# check for idle kernels every two minutes
+c.MappingKernelManager.cull_interval = 2 * 60
+
 
 # Remove containers once they are stopped
 c.DockerSpawner.remove_containers = True

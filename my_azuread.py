f4f4364fbd9b9fed71cb97bdb96e0748c872afde
bh0fer <lebalz@outlook.com>
2026-03-21 16:05:13 +0100

    try fix

--- my_azuread.py
+++ my_azuread.py
@@ -2,9 +2,6 @@
 Custom Authenticator to use Azure AD with JupyterHub
 
 """
-import json
-import jwt
-import urllib
 # import logging
 # logging.basicConfig(filename='example_jwt.log', encoding='utf8', level=logging.DEBUG)
 
@@ -12,15 +9,6 @@ from jupyterhub.auth import LocalAuthenticator
 
 from traitlets import default
 from .azuread import AzureAdOAuthenticator
-
-
-def azure_token_url_for(tentant):
-    return 'https://login.microsoftonline.com/{0}/oauth2/token'.format(tentant)
-
-
-def azure_authorize_url_for(tentant):
-    return 'https://login.microsoftonline.com/{0}/oauth2/authorize'.format(
-        tentant)
 
 
 def sanitized_username(raw):
@@ -53,49 +41,6 @@ class MyAzureAdOAuthenticator(AzureAdOAuthenticator):
         Override normalize_username to avoid lowercasing usernames
         """
         return sanitized_username(username)
-
-    async def authenticate(self, handler, data=None):
-        code = handler.get_argument("code")
-
-        params = dict(
-            client_id=self.client_id,
-            client_secret=self.client_secret,
-            grant_type='authorization_code',
-            code=code,
-            redirect_uri=self.get_callback_url(handler))
-
-        body = urllib.parse.urlencode(params).encode('utf-8')
-
-        url = azure_token_url_for(self.tenant_id)
-
-        headers = {
-            'Content-Type':
-            'application/x-www-form-urlencoded; charset=UTF-8'
-        }
-
-        resp_json = await self.httpfetch(
-            url,
-            method="POST",
-            headers=headers,
-            body=body,
-            label="fetching access token",
-        )
-
-        # app_log.info("Response %s", resp_json)
-        access_token = resp_json['access_token']
-
-        id_token = resp_json['id_token']
-        decoded = jwt.decode(id_token, options={"verify_signature": False})
-        cleaned_name = sanitized_username(decoded[self.username_claim])
-
-        userdict = {"name": cleaned_name}
-
-        userdict["auth_state"] = auth_state = {}
-        auth_state['access_token'] = access_token
-        # results in a decoded JWT for the user data
-        auth_state['user'] = decoded
-
-        return userdict
 
 
 class LocalMyAzureAdOAuthenticator(LocalAuthenticator, MyAzureAdOAuthenticator):
